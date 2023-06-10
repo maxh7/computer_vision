f9046f6dfb87ce958eb2d9a3615a95270ac5bd09
mAx <62607103+maxh7@users.noreply.github.com>
2023-06-10 15:53:20 +0200

    ed

--- recognition.py
+++ recognition.py
@@ -5,7 +5,6 @@ import numpy as np
 import math
 
 
-
 def face_confidence(face_distance, face_match_threshold=0.6):
     range = (1.0 - face_match_threshold)
     linear_val = (1.0 - face_distance) / (range * 2.0)
